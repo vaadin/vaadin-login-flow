d09d0a4ec87ab70bcce40cc53c633db61e91d809
alexberazouski <alex@vaadin.com>
2018-12-05 11:58:39 +0200

    Remove unused imports [skip ci]

--- vaadin-login-flow-integration-tests/src/main/java/com/vaadin/flow/component/login/examples/OverlayView.java
+++ vaadin-login-flow-integration-tests/src/main/java/com/vaadin/flow/component/login/examples/OverlayView.java
@@ -1,8 +1,6 @@
 package com.vaadin.flow.component.login.examples;
 
 import com.vaadin.flow.component.html.NativeButton;
-import com.vaadin.flow.component.login.AbstractLogin;
-import com.vaadin.flow.component.login.Login;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.router.Route;
 
