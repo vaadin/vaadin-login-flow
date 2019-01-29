5fdd0359f175154e82370733e55f626db8e2b9b7
alexberazouski <alex@vaadin.com>
2019-01-29 16:40:06 +0200

    Return back the Route annotation removed by mistake

--- vaadin-login-flow-integration-tests/src/main/java/com/vaadin/flow/component/login/examples/MainView.java
+++ vaadin-login-flow-integration-tests/src/main/java/com/vaadin/flow/component/login/examples/MainView.java
@@ -1,7 +1,9 @@
 package com.vaadin.flow.component.login.examples;
 
 import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.router.Route;
 
+@Route
 public class MainView extends AbstractView {
 
     private LoginForm loginForm = new LoginForm();
